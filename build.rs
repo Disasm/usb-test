903feed930e55e56af3fd109be1b10370eb57f22
Vadim Kaushan <admin@disasm.info>
2019-03-26 19:23:15 +0300

    Use endpoint allocator

--- build.rs
+++ build.rs
@@ -2,32 +2,32 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
-use usb_device_generator::{UsbDirection, EndpointAddress};
+use usb_device_generator::{UsbDirection, EndpointType};
 use usb_device_generator::builder::{EndpointBuilder, DeviceBuilder, UsbVidPid};
 use usb_device_generator::generator::{generate_file, DeviceEndpoint};
 use usb_device_generator::cdc::{USB_CLASS_CDC, create_cdc_function};
+use usb_device_generator::endpoint::{DeviceAllocator, EndpointBuilderEx, DeviceBuilderEx};
 
-fn create_cdc_device() -> DeviceBuilder {
-    // TODO: allocate these
-    let _comm_ep_addr = EndpointAddress::from_parts(1, UsbDirection::In);
-    let _read_ep_addr = EndpointAddress::from_parts(2, UsbDirection::Out);
-    let _write_ep_addr = EndpointAddress::from_parts(2, UsbDirection::In);
-    let comm_ep: DeviceEndpoint = EndpointBuilder::new().input().interrupt().number(1).max_packet_size(8).interval(255).into();
-    let read_ep: DeviceEndpoint = EndpointBuilder::new().output().bulk().number(2).max_packet_size(64).into();
-    let write_ep: DeviceEndpoint = EndpointBuilder::new().input().bulk().number(2).max_packet_size(64).into();
 
+fn create_cdc_device(allocator: &mut DeviceAllocator) -> DeviceBuilder {
     let mut device = DeviceBuilder::new(UsbVidPid(0x5824, 0x27dd))
         .manufacturer("Fake company")
         .product("Serial port")
         .serial_number("TEST")
-        .device_class(USB_CLASS_CDC);
+        .device_class(USB_CLASS_CDC)
+        .allocate(allocator);
+
+    let comm_ep: DeviceEndpoint = EndpointBuilder::new().direction(UsbDirection::In).ep_type(EndpointType::Interrupt).max_packet_size(8).interval(255).allocate(allocator).into();
+    let read_ep: DeviceEndpoint = EndpointBuilder::new().direction(UsbDirection::Out).ep_type(EndpointType::Bulk).max_packet_size(64).allocate(allocator).into();
+    let write_ep: DeviceEndpoint = EndpointBuilder::new().direction(UsbDirection::In).ep_type(EndpointType::Bulk).max_packet_size(64).allocate(allocator).into();
 
     create_cdc_function(&mut device, comm_ep, read_ep, write_ep);
     device
 }
 
 fn build_device(out_dir: &PathBuf) {
-    let d = create_cdc_device();
+    let mut allocator = DeviceAllocator::new();
+    let d = create_cdc_device(&mut allocator);
     let config = d.build();
     generate_file(out_dir.join("generated.rs"), config).unwrap();
 }
